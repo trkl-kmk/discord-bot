a7926a723a4a96b741aa42e3dc9de34be7f65cb2
Gabriel <gabriellikov@gmail.com>
2023-08-24 23:03:03 +0300

    Testingg

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-import 'dotenv/config';
-import express from 'express';
+import "dotenv/config";
+import express from "express";
 import {
-  InteractionType,
-  InteractionResponseType,
-  InteractionResponseFlags,
-  MessageComponentTypes,
-  ButtonStyleTypes,
-} from 'discord-interactions';
-import { VerifyDiscordRequest, getRandomEmoji } from './utils.js';
+	InteractionType,
+	InteractionResponseType,
+	InteractionResponseFlags,
+	MessageComponentTypes,
+	ButtonStyleTypes,
+} from "discord-interactions";
+import { VerifyDiscordRequest, getRandomEmoji } from "./utils.js";
 
 // Create an express app
 const app = express();
@@ -22,38 +22,48 @@ const activeGames = {};
 /**
  * Interactions endpoint URL where Discord will send HTTP requests
  */
-app.post('/interactions', async function (req, res) {
-  // Interaction type and data
-  const { type, id, data } = req.body;
+app.post("/interactions", async function (req, res) {
+	// Interaction type and data
+	const { type, id, data } = req.body;
 
-  /**
-   * Handle verification requests
-   */
-  if (type === InteractionType.PING) {
-    return res.send({ type: InteractionResponseType.PONG });
-  }
+	/**
+	 * Handle verification requests
+	 */
+	if (type === InteractionType.PING) {
+		return res.send({ type: InteractionResponseType.PONG });
+	}
 
-  /**
-   * Handle slash command requests
-   * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
-   */
-  if (type === InteractionType.APPLICATION_COMMAND) {
-    const { name } = data;
+	/**
+	 * Handle slash command requests
+	 * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
+	 */
+	if (type === InteractionType.APPLICATION_COMMAND) {
+		const { name } = data;
 
-    // "test" command
-    if (name === 'test') {
-      // Send a message into the channel where command was triggered from
-      return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          // Fetches a random emoji to send from a helper function
-          content: 'hello world ' + getRandomEmoji(),
-        },
-      });
-    }
-  }
+		// "test" command
+		if (name === "test") {
+			// Send a message into the channel where command was triggered from
+			return res.send({
+				type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+				data: {
+					// Fetches a random emoji to send from a helper function
+					content: "hello world " + getRandomEmoji(),
+				},
+			});
+		}
+		if (name === "PUM") {
+			// Send a message into the channel where command was triggered from
+			return res.send({
+				type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+				data: {
+					// Fetches a random emoji to send from a helper function
+					content: "PUMDADADADAD" + getRandomEmoji(),
+				},
+			});
+		}
+	}
 });
 
 app.listen(PORT, () => {
-  console.log('Listening on port', PORT);
+	console.log("Listening on port", PORT);
 });
--- commands.js
+++ commands.js
@@ -1,13 +1,18 @@
-import 'dotenv/config';
-import { InstallGlobalCommands } from './utils.js';
+import "dotenv/config";
+import { InstallGlobalCommands } from "./utils.js";
 
 // Simple test command
 const TEST_COMMAND = {
-  name: 'test',
-  description: 'Basic command',
-  type: 1,
+	name: "test",
+	description: "Basic command",
+	type: 1,
+};
+const PUMDADA = {
+	name: "PUM",
+	description: "DADA",
+	type: 1,
 };
 
-const ALL_COMMANDS = [TEST_COMMAND];
+const ALL_COMMANDS = [TEST_COMMAND, PUMDADA];
 
 InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
