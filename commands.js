8cc3d7af03651223aaf95085b0683a5ebb0a8471
Uiyx <uiyx@disroot.org>
2023-08-25 00:06:08 +0300

    zabravih

--- commands.js
+++ commands.js
@@ -3,21 +3,21 @@ import { InstallGlobalCommands } from "./utils.js";
 
 // Simple test command
 const TEST_COMMAND = {
-	name: "test",
-	description: "Basic command",
-	type: 1,
+  name: "test",
+  description: "Basic command",
+  type: 1,
 };
 const PUMDADA = {
-	name: "pum",
-	description: "DADA",
-	type: 1,
+  name: "pum",
+  description: "DADA",
+  type: 1,
 };
-const Vibo = {
-	name: "vibosin",
-	description: "Vibo",
-	type: 1,
+const VIBO = {
+  name: "vibosin",
+  description: "Vibo",
+  type: 1,
 };
 
-const ALL_COMMANDS = [TEST_COMMAND, PUMDADA, Vibo];
+const ALL_COMMANDS = [TEST_COMMAND, PUMDADA, VIBO];
 
 InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
