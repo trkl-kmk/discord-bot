b5d1d4d91d1b65a775f7e0a325867b6bc05e0080
Uiyx <uiyx@disroot.org>
2023-08-24 23:51:16 +0300

    pumdadada

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.post("/interactions", async function (req, res) {
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
           // Fetches a random emoji to send from a helper function
-          content: "PUMDADA" + getRandomEmoji(),
+          content: "PUMDADADA" + getRandomEmoji(),
         },
       });
     }
