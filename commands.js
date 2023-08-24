5f6bdf05f68f96174bc05fa87cbda9f22bd77da2
Uiyx <uiyx@disroot.org>
2023-08-24 22:05:17 +0300

    initial commmit

---
+++ commands.js
@@ -0,0 +1,28 @@
+import 'dotenv/config';
+import { capitalize, InstallGlobalCommands } from './utils.js';
+
+// Get the game choices from game.js
+function createCommandChoices() {
+  const choices = getRPSChoices();
+  const commandChoices = [];
+
+  for (let choice of choices) {
+    commandChoices.push({
+      name: capitalize(choice),
+      value: choice.toLowerCase(),
+    });
+  }
+
+  return commandChoices;
+}
+
+// Simple test command
+const TEST_COMMAND = {
+  name: 'test',
+  description: 'Basic command',
+  type: 1,
+};
+
+const ALL_COMMANDS = [TEST_COMMAND];
+
+InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
